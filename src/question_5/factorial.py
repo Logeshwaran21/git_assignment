d1f71e38d65101f5a159d5363e8cb7f16b68976b
Logeshwaran21 <logeshwaran.a@diggibyte.com>
2024-03-09 10:39:15 +0530

    factorial modified_2

--- src/question_5/factorial.py
+++ src/question_5/factorial.py
@@ -1,17 +1,12 @@
 def factorial(n):
-    if n < 0:
-        return 0
-    elif n == 0 or n == 1:
-        return 1
-    else:
-        fact = 1
-        while (n > 1):
-            fact *= n
-            n -= 1
-        return fact
+    res = 1
+
+    for i in range(2, n + 1):
+        res *= i
+    return res
 
 
 # Driver Code
 num = 5
 print("Factorial of", num, "is",
-      factorial(num))
+      factorial(num)
