1d21420a97a2487705da8884fc3393b415d81bae
Logeshwaran21 <logeshwaran.a@diggibyte.com>
2024-03-09 10:48:38 +0530

    conflict resolvd

--- src/question_5/factorial.py
+++ src/question_5/factorial.py
@@ -1,11 +1,13 @@
 def factorial(n):
+
     res = 1
 
-    for i in range(2, n + 1):
+    for i in range(2, n+1):
         res *= i
     return res
-
-
-# Driver Code
+ # Driver Code
 num = 5
-print("Factorial of", num, "is", factorial(num))
+print("Factorial of", num, "is",
+factorial(num))
+
+
